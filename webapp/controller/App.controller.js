b37865bbba2d7925bb1cc887fe4d3c338991c92f
CLinny <marina.boppre@hotmail.com>
2017-12-30 10:13:44 +0000

    Walkthrough Step 7

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,20 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function(Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, MessageToast, JSONModel) {
 	"use strict";
 	return Controller.extend("clinny.tutorial.wt.controller.App", {
+		onInit : function () {
+			var oData = {
+				chuddel: "Super Chuddel",
+				recipient : {
+					name : "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+		},
 		onShowHello : function () {
 			MessageToast.show("Hello World!");
 		}
