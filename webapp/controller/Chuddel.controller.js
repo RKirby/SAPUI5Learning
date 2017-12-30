4c01b965c3a74f46140acac867fcd79f3627b04c
CLinny <marina.boppre@hotmail.com>
2017-12-30 09:49:42 +0000

    Walkthrough Step 5 Extension

--- webapp/controller/Chuddel.controller.js
+++ webapp/controller/Chuddel.controller.js
@@ -2,9 +2,9 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function(Controller) {
 	"use strict";
-	return Controller.extend("clinny.tutorial.wt.controller.App", {
+	return Controller.extend("clinny.tutorial.wt.controller.Chuddel", {
 		onShowHello : function () {
-			alert("Hello Wolrd!");
+			alert("Hello Chuddel!");
 		}
 	});
 });
